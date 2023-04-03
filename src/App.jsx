f7fa30267e0bc97dd83688471af2c4609a8e0139
rschreie3 <97309686+rschreie3@users.noreply.github.com>
2023-04-03 00:29:26 -0400

    changed appbar to a darkcyan background

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,18 @@ import logo from "./logo.svg";
 import "./App.css";
 import { HashRouter, Routes, Route } from "react-router-dom";
 import { Home } from "./components/HomePage/Home";
+import { Box } from "@mui/material";
+import Header from "./components/header/header.jsx";
 
 function App() {
   return (
     <HashRouter>
+      <Header />
       <Routes>
         <Route path="/" element={<Home />} />
+        <Route path="/write-paper" element={<></>} />
+        <Route path="/tools" element={<></>} />
+        <Route path="/bibliography" element={<></>} />
       </Routes>
     </HashRouter>
   );
