696f4bbdc967c111380a06da286729caa777899a
rschreie3 <97309686+rschreie3@users.noreply.github.com>
2023-04-05 01:07:04 -0400

    layout of tools page, working on openai api

--- src/components/PaperToolsPage/tools.jsx
+++ src/components/PaperToolsPage/tools.jsx
@@ -1,34 +1,70 @@
 import React, { useState, useContext } from "react";
 import OpenAIAPI from "react-openai-api";
-import { Grid } from "@mui/material";
 import { Box } from "@mui/material";
 import { OutlinedInput } from "@mui/material";
 import { Button } from "@mui/material";
+import { TextField } from "@mui/material";
 
 export const Tools = () => {
   const [input, setInput] = useState("");
+  const [results, setResults] = useState("");
 
   function onInput(event) {
     setInput(event.target.value);
   }
 
+  function handleDefine() {
+    // <OpenAIAPI
+    //   apiKey="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"
+    //   payload={{
+    //     prompt: "Define " + input,
+    //     maxTokens: 25,
+    //     temperature: 0.2,
+    //     n: 1,
+    //   }}
+    //   responseHandler={() => defineResponseHandler()}
+    // />;
+  }
+
+  function handleSyn() {}
+
+  function handleAnt() {}
+
   return (
     <Box
       component="form"
       sx={{
-        margin: "2vh",
+        margin: "2",
       }}
     >
-      <OutlinedInput
-        multiline
-        placeholder="Enter text here..."
-        onInput={onInput}
-        color="primary"
-      />
-      <Button>Translate</Button>
-      <Button>Define</Button>
-      <Button>Synonyms</Button>
-      <Button>Antonyms</Button>
+      <div align="center">
+        <OutlinedInput
+          multiline
+          placeholder="Enter text here..."
+          onInput={onInput}
+          sx={{
+            margin: 2,
+          }}
+        />
+      </div>
+
+      <div align="center">
+        <Button onClick={handleDefine}>Define</Button>
+        <Button onClick={handleSyn}>Synonyms</Button>
+        <Button onClick={handleAnt}>Antonyms</Button>
+      </div>
+
+      <div align="center">
+        <TextField
+          multiline
+          minRows={8}
+          sx={{
+            margin: 4,
+            minWidth: "75vh",
+          }}
+          value={results}
+        />
+      </div>
     </Box>
   );
 };
