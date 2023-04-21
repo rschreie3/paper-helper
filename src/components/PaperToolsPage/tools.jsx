7acbb585a1cf3ceee2ae348961d32e6fffcb684e
rschreie3 <97309686+rschreie3@users.noreply.github.com>
2023-04-20 23:41:06 -0400

    got tools page working with open ai

--- src/components/PaperToolsPage/tools.jsx
+++ src/components/PaperToolsPage/tools.jsx
@@ -7,23 +7,56 @@ import { TextField } from "@mui/material";
 export const Tools = () => {
   const [input, setInput] = useState("");
   const [results, setResults] = useState("");
+  const [response, setResponse] = useState("");
 
   function onInput(event) {
     setInput(event.target.value);
   }
 
-  // function handleDefine() {
-  //   <OpenAIAPI
-  //     apiKey="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"
-  //     payload={{
-  //       prompt: "Define " + input,
-  //       maxTokens: 25,
-  //       temperature: 0.2,
-  //       n: 1,
-  //     }}
-  //     responseHandler={() => defineResponseHandler()}
-  //   />;
-  // }
+  const createRequest = async (props) => {
+    const apiKey = "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";
+    const url =
+      "https://api.openai.com/v1/engines/text-davinci-003/completions";
+
+    // const myPrompt = () => {
+    //   if (props.option === "define") {
+    //     return `Dictionary definition of "${input}"`;
+    //   } else if (props.option === "synonyms") {
+    //     return `Give 10 synonyms for "${input}"`;
+    //   } else if (props.option === "antonyms") {
+    //     return `Give 10 synonyms for "${input}"`;
+    //   } else {
+    //     return `What is 5 + 5`;
+    //   }
+    // };
+
+    const prompt =
+      props.option === "define"
+        ? `Dictionary definition of "${input}"`
+        : props.option === "synonyms"
+        ? `Give 10 synonyms for "${input}"`
+        : props.option === "antonyms"
+        ? `Give 10 synonyms for "${input}"`
+        : props.option === "sentence"
+        ? `Use "${input}" in a sentence`
+        : "Give me the infinity symbol";
+
+    const response = await fetch(url, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: `Bearer ${apiKey}`,
+      },
+      body: JSON.stringify({
+        prompt: prompt,
+        max_tokens: 100,
+      }),
+    });
+
+    const data = await response.json();
+    const definition = data.choices[0].text;
+    setResponse(definition);
+  };
 
   function handleSyn() {}
 
@@ -48,9 +81,18 @@ export const Tools = () => {
       </div>
 
       <div align="center">
-        {/* <Button onClick={handleDefine}>Define</Button>
-        <Button onClick={handleSyn}>Synonyms</Button>
-        <Button onClick={handleAnt}>Antonyms</Button> */}
+        <Button onClick={() => createRequest({ option: "define" })}>
+          Define
+        </Button>
+        <Button onClick={() => createRequest({ option: "sentence" })}>
+          Sentence
+        </Button>
+        <Button onClick={() => createRequest({ option: "synonyms" })}>
+          Synonyms
+        </Button>
+        <Button onClick={() => createRequest({ option: "antonyms" })}>
+          Antonyms
+        </Button>
       </div>
 
       <div align="center">
@@ -60,8 +102,9 @@ export const Tools = () => {
           sx={{
             margin: 4,
             minWidth: "75vh",
+            alignContent: "center",
           }}
-          value={results}
+          value={response}
         />
       </div>
     </Box>
